d5d0bceddbaf133d9111d57ed6025b71ea8f9374
akudryavtseva <elenka_k@mail.ru>
2020-02-22 22:19:16 +0300

    ArrayStorage implementation

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,68 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        int k = 0;
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].toString().equals(uuid)) {
+                k = i;
+                break;
+            } else k = -1;
+        }
+
+        if (k != -1) {
+            return storage[k];
+        } else return null;
     }
 
     void delete(String uuid) {
+        int k = 0;
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].toString().equals(uuid)) {
+                k = i;
+                break;
+            } else {
+                k = -1;
+            }
+        }
+        if (k != -1) {
+            for (int j = k; j < size(); j++) {
+                storage[j] = storage[j + 1];
+                storage[j + 1] = null;
+            }
+        } else System.out.println("Неверный uuid.");
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] newStorage = new Resume[size()];
+        for (int i = 0; i < size(); i++) {
+            newStorage[i] = storage[i];
+        }
+        return newStorage;
     }
 
     int size() {
-        return 0;
+        int s = 0;
+        for (Resume element : storage) {
+            if (element != null) {
+                s++;
+            }
+        }
+        return s;
     }
 }
