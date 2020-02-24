6f8b8177acdd79f0b7d504924788cbc953d4b391
akudryavtseva <elenka_k@mail.ru>
2020-02-24 21:48:33 +0300

    ArrayStorage implementation2

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,67 +1,42 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    public ArrayList<Resume> listResume = new ArrayList<>();
 
     void clear() {
-        int storageSize = size();
-        Arrays.fill(storage, 0, storageSize, null);
+        listResume.clear();
     }
 
     void save(Resume r) {
-        int storageSize = size();
-        storage[storageSize] = r;
+        listResume.add(r);
     }
 
     Resume get(String uuid) {
-        int storageSize = size();
-        for (int i = 0; i < storageSize; i++) {
-            if (storage[i].toString().equals(uuid)) {
-                return storage[i];
+        Resume[] resumes = listResume.toArray(new Resume[0]);
+        for (Resume r : resumes) {
+            if (r.toString().equals(uuid)) {
+                return r;
             }
         }
         return null;
     }
 
     void delete(String uuid) {
-        int storageSize = size();
-        int k = -1;
-        for (int i = 0; i < storageSize; i++) {
-            if (storage[i].toString().equals(uuid)) {
-                k = i;
-                break;
-            }
-        }
-        if (k >= 0) {
-            for (int j = k; j < storageSize; j++) {
-                storage[j] = storage[j + 1];
-            }
-            storage[storageSize] = null;
-        } else System.out.println("Неверный uuid.");
+        Resume u = this.get(uuid);
+        listResume.remove(u);
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int storageSize = size();
-        Resume[] resumes = new Resume[storageSize];
-        for (int i = 0; i < storageSize; i++) {
-            resumes[i] = storage[i];
-        }
-        return resumes;
+        return listResume.toArray(new Resume[0]);
     }
 
     int size() {
-        int s = 0;
-        for (Resume element : storage) {
-            if (element != null) {
-                s++;
-            }
-        }
-        return s;
+        return listResume.size();
     }
 }
